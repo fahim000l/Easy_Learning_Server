e09e9cf06da6f8f05158b1141eb6e1797205acea
Md Fahim Faisal <mdfahimfaisal000@gmail.com>
2022-10-25 20:44:58 +0600

    updated

--- index.js
+++ index.js
@@ -19,13 +19,19 @@ app.get('/courses', (req, res) => {
     res.send(courses);
 });
 app.get('/course_category/:id', (req, res) => {
-    const id = req.params.category_id;
-    const selectedCategory = courses.filter(course => course.id == id);
-    res.send(selectedCategory);
+    const id = req.params.id;
+    if (id == 7) {
+        res.send(courses)
+    }
+    else {
+
+        const selectedCategory = courses.filter(course => course.category_id == id);
+        res.send(selectedCategory);
+    }
 });
 app.get('/courses/:id', (req, res) => {
-    const id = req.params._id;
-    const selectedCourse = courses.find(course => course.id == id);
+    const id = req.params.id;
+    const selectedCourse = courses.find(course => course._id == id);
     res.send(selectedCourse);
 })
 
