7d265955cf88eaadcc00ee147d08bd6f88114638
Md Fahim Faisal <mdfahimfaisal000@gmail.com>
2022-10-25 13:39:51 +0600

    server done

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const course_category = require('./Data/category.json');
-conat courses = require('./Data/course.json');
+const courses = require('./Data/course.json');
 
 app.get('/', (req, res) => {
     res.send('Self Learning');
@@ -15,6 +15,19 @@ app.get('/', (req, res) => {
 app.get('/course_category', (req, res) => {
     res.send(course_category);
 });
+app.get('/courses', (req, res) => {
+    res.send(courses);
+});
+app.get('/course_category/:id', (req, res) => {
+    const id = req.params.category_id;
+    const selectedCategory = courses.filter(course => course.id == id);
+    res.send(selectedCategory);
+});
+app.get('/courses/:id', (req, res) => {
+    const id = req.params._id;
+    const selectedCourse = courses.find(course => course.id == id);
+    res.send(selectedCourse);
+})
 
 app.listen(port, () => {
     console.log('Self Learning is running on port :', port);
